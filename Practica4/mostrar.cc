0a02eec975bbbb4d27240427f0a4c51897e46859
ManuCbr13 <i72cadem@uco.com>
2018-12-06 18:56:45 +0100

    mostrar.cc

---
+++ Practica4/mostrar.cc
@@ -0,0 +1,26 @@
+void Agenda::mostrar(string cadena){
+	list<Alumno>::iterator a1;
+	for(a1=agenda_.begin();a1!=agenda_.end();a1++){
+		if(buscar((cadena))==1){
+			string cadena=(*a1).getDNI()+".md";
+			ofstream ficherosalida(cadena.c_str());
+			ficherosalida<<"** DNI : "<<(*a1).getDNI()<<endl;
+			ficherosalida<<"** Nombre : "<<(*a1).getNombre()<<endl;
+			ficherosalida<<"** Apellidos : "<<(*a1).getApellidos()<<endl;
+			ficherosalida<<"** Direccion : "<<(*a1).getDireccion()<<endl;
+			ficherosalida<<"** Telefono : "<<(*a1).getTelefono()<<endl;
+			ficherosalida<<"** Email : "<<(*a1).getEmail()<<endl;
+			ficherosalida<<"** Curso : "<<(*a1).getCurso()<<endl;
+			ficherosalida<<"** Fecha de nacimiento : "<<(*a1).getFecha()<<endl;
+			ficherosalida<<"** Equipo : "<<(*a1).getEquipo()<<endl;
+			if((*a1).getLider()==1){
+				ficherosalida<<"** Lider : SI "<<endl;
+			}
+			else{
+				ficherosalida<<"** Lider : NO "<<endl;
+			}
+			ficherosalida.close();
+		}
+	}
+
+}
