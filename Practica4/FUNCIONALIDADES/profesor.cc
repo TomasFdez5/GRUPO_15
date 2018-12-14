2244b6e60a1edc2c387f6547d6b077bef65cc021
TFU <i72feurt@uco.es>
2018-12-15 00:26:47 +0100

    Funciones registrar y logear

--- Practica4/FUNCIONALIDADES/profesor.cc
+++ Practica4/FUNCIONALIDADES/profesor.cc
@@ -11,3 +11,70 @@ Profesor::Profesor(char* id, char* password, int rol){
 	strcpy(password_,password);
 	role_=rol;
 }
+
+
+int Profesor::Logearse(Profesor aux){
+ 	struct registro read;
+ 	ifstream Profesores("Profesores.bin",ios::in| ios::binary);
+
+ 	if(Profesores.is_open()){
+ 		while(!Profesores.eof()){
+			Profesores.read((char*)&read,sizeof(registro));
+ 			if(strcmp(read.ID,aux.id_)==0){
+ 				if(strcmp(read.password,aux.password_)==0){
+ 					if((aux.role_==read.rol) && (aux.role_==1)){
+ 						return 2;
+ 					}else{
+						if((aux.role_==read.rol)&& (aux.role_==0)){
+							return 1;
+						}else{
+							if(aux.role_!=read.rol){
+								return -1;
+							}
+						}
+
+					}
+
+ 				}
+ 				else{
+ 					return 0;
+ 				}
+ 			}
+
+ 		}
+
+	}
+ 	Profesores.close();
+ 	return 0;
+}
+
+
+int Profesor::Registrarse(Profesor aux){
+
+	int encontrado=0;
+	struct registro read;
+	ifstream Profesores("Profesores.bin", ios::in|ios::binary);
+	Profesores.seekg(0, ios::beg);
+
+	if(Profesores.is_open()){
+
+ 		while(!Profesores.eof()){
+
+			Profesores.read((char*)&read,sizeof(registro));
+ 			if(strcmp(read.ID,aux.id_)==0){
+ 				encontrado=1;
+ 				Profesores.close();
+ 				return -1;
+ 			}
+ 		}
+	}
+	Profesores.close();
+	if(encontrado==0){
+		ofstream Profesores("Profesores.bin", ios::out|ios::binary);
+		Profesores.seekp(0, ios::end);
+		Profesores.write((char*)&aux, sizeof(registro));
+		Profesores.close();
+
+	}
+	return encontrado;
+}
