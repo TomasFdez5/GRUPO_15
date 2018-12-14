a24dd0ec32f1a1e48a68bc6744157017270e58ca
TFU <i72feurt@uco.es>
2018-12-15 00:17:04 +0100

    Funciones buscar alumno

--- Practica4/FUNCIONALIDADES/agenda.cc
+++ Practica4/FUNCIONALIDADES/agenda.cc
@@ -22,3 +22,40 @@ void Agenda::insertar(Alumno alumno){
 	}
 	agenda_.push_back(alumno);
 }
+
+
+int Agenda::buscar(string dni){
+	list<Alumno>::iterator a1;
+	int encontrado=0;
+	for(a1=agenda_.begin();a1!=agenda_.end();a1++){
+		if(dni==a1->getDNI()){
+			encontrado=1;
+		}
+	}
+
+	return encontrado;
+}
+int Agenda::buscar1(string apellidos){
+	int encontrado=0;
+	list<Alumno>::iterator a1;
+	for(a1=agenda_.begin();a1!=agenda_.end();a1++){
+		if(apellidos==a1->getApellidos()){
+			encontrado=1;
+		}
+	}
+
+	return encontrado;
+}
+
+int Agenda::buscar2(int equipo){
+	list<Alumno>::iterator a1;
+	int encontrado=0;
+	for(a1=agenda_.begin();a1!=agenda_.end();a1++){
+		if(equipo==a1->getEquipo()){
+			encontrado=1;
+
+		}
+	}
+
+	return encontrado;
+}
