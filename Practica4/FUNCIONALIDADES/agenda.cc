6da96d6c77810ce27f66131c7567b6d5ecc90db7
TFU <i72feurt@uco.es>
2018-12-15 00:21:02 +0100

    Funciones borrar alumno

--- Practica4/FUNCIONALIDADES/agenda.cc
+++ Practica4/FUNCIONALIDADES/agenda.cc
@@ -143,3 +143,34 @@ int Agenda::mostrar_todos(int seleccion,int orden){
 	ficherosalida.close();
 	return 1;
 }
+
+
+int Agenda::borrar(string dni){
+	list<Alumno>::iterator a1;
+
+	if(agenda_.empty()){
+		return -1;
+	}
+	for(a1=agenda_.begin(); a1!=agenda_.end();a1++){
+		if(dni==a1->getDNI()){
+			agenda_.erase(a1);
+			return 1;
+		}
+	}
+	return 2;
+}
+
+int Agenda::borrar1(string apellidos){
+	list<Alumno>::iterator a1;
+
+	if(agenda_.empty()){
+		return -1;
+	}
+	for(a1=agenda_.begin(); a1!=agenda_.end();a1++){
+		if(apellidos==a1->getApellidos()){
+			agenda_.erase(a1);
+			return 1;
+		}
+	}
+	return 2;
+}
