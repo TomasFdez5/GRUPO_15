0fbb654b560ba64964db49bd5baa2e19a3395989
TFU <i72feurt@uco.es>
2018-12-15 00:18:23 +0100

    Funciones mostrar y mostrar todos

--- Practica4/FUNCIONALIDADES/agenda.cc
+++ Practica4/FUNCIONALIDADES/agenda.cc
@@ -59,3 +59,74 @@ int Agenda::buscar2(int equipo){
 
 	return encontrado;
 }
+
+
+
+void Agenda::mostrar(string cadena){
+	list<Alumno>::iterator a1;
+	for(a1=agenda_.begin();a1!=agenda_.end();a1++){
+		if(a1->getDNI()==cadena&&buscar((cadena))==1){
+			string cadena=(*a1).getDNI()+".md";
+			ofstream ficherosalida(cadena.c_str());
+			ficherosalida<<"** DNI : "<<(*a1).getDNI()<<endl;
+			ficherosalida<<"** Nombre : "<<(*a1).getNombre()<<endl;
+			ficherosalida<<"** Apellidos : "<<(*a1).getApellidos()<<endl;
+			ficherosalida<<"** Direccion : "<<(*a1).getDireccion()<<endl;
+			ficherosalida<<"** Telefono : "<<(*a1).getTelefono()<<endl;
+			ficherosalida<<"** Email : "<<(*a1).getEmail()<<endl;
+			ficherosalida<<"** Curso : "<<(*a1).getCurso()<<endl;
+			ficherosalida<<"** Fecha de nacimiento : "<<(*a1).getFecha()<<endl;
+			ficherosalida<<"** Equipo : "<<(*a1).getEquipo()<<endl;
+			if((*a1).getLider()==1){
+				ficherosalida<<"** Lider : SI "<<endl;
+			}
+			else{
+				ficherosalida<<"** Lider : NO "<<endl;
+			}
+			imprimir(*a1);
+			ficherosalida.close();
+		}
+	}
+
+}
+
+
+int Agenda::mostrar_todos(int seleccion,int orden){
+	ofstream ficherosalida("agenda.md");
+	list<Alumno>::iterator a1;
+	int i = 1;
+	if(agenda_.empty()){
+		ficherosalida.close();
+		return 0;
+	}
+	if(seleccion==1&&orden==1){agenda_.sort(nombreASC);}
+	if(seleccion==1&&orden==2){agenda_.sort(nombreDESC);}
+	if(seleccion==2&&orden==1){agenda_.sort(apellidoASC);}
+	if(seleccion==2&&orden==2){agenda_.sort(apellidoDESC);}
+	if(seleccion==3&&orden==1){agenda_.sort(dniASC);}
+	if(seleccion==3&&orden==2){agenda_.sort(dniDESC);}
+	if(seleccion==2&&orden==1){agenda_.sort(equipoASC);}
+	if(seleccion==2&&orden==2){agenda_.sort(equipoDESC);}
+
+	for(a1=agenda_.begin();a1!=agenda_.end();a1++){
+		ficherosalida<<"### Alumno "<<i<<endl;
+		ficherosalida<<"** DNI : "<<(*a1).getDNI()<<endl;
+		ficherosalida<<"** Nombre : "<<(*a1).getNombre()<<endl;
+		ficherosalida<<"** Apellidos : "<<(*a1).getApellidos()<<endl;
+		ficherosalida<<"** Direccion : "<<(*a1).getDireccion()<<endl;
+		ficherosalida<<"** Telefono : "<<(*a1).getTelefono()<<endl;
+		ficherosalida<<"** Email : "<<(*a1).getEmail()<<endl;
+		ficherosalida<<"** Curso : "<<(*a1).getCurso()<<endl;
+		ficherosalida<<"** Fecha de nacimiento : "<<(*a1).getFecha()<<endl;
+		ficherosalida<<"** Equipo : "<<(*a1).getEquipo()<<endl;
+		if((*a1).getLider()==1){
+			ficherosalida<<"** Lider : SI "<<endl;
+		}
+		else{
+			ficherosalida<<"** Lider : NO "<<endl;
+		}
+		i++;
+	}
+	ficherosalida.close();
+	return 1;
+}
